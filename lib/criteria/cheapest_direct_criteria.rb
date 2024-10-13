2e7593a0dff642492255313abccd45ed9bfb6539
jaymeweiss <jayme.weiss@zappistore.com>
2024-10-13 19:41:38 +0200

    add cheapest direct

---
+++ lib/criteria/cheapest_direct_criteria.rb
@@ -0,0 +1,16 @@
+module Criteria
+  class CheapestDirectCriteria < Criteria
+    def matching_routes
+      sorted_routes = all_possible_routes.sort_by(&:price_in_euro)
+      return [] if sorted_routes.empty?
+      cheapest_price = sorted_routes.first.price_in_euro
+      sorted_routes.select { |route| route.price_in_euro == cheapest_price }
+    end
+
+    def all_possible_routes
+      # find all the possible starting points to search for routes from
+      origin_options = ShippingOption.where(origin_port: @origin_port, destination_port: @destination_port)
+      origin_options.map { |option| Route.new([option]) }
+    end
+  end
+end
---
+++ test/lib/criteria/cheapest_direct_criteria_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class CheapestDirectCriteriaTest < ActiveSupport::TestCase
+  fixtures :shipping_options, :exchange_rates, :rates
+
+  test "should return the cheapest route if there is only 1 with the matching criteria" do
+    criteria = Criteria::CheapestDirectCriteria.new("CNSHA", "NLRTM")
+    routes = criteria.matching_routes
+    assert_equal 1, routes.size
+    assert_equal ["MNOP"], routes[0].stops.map { |stop| stop.sailing_code }.sort
+    assert_equal 1, routes[0].stops.count
+  end
+
+  test "should return nothing if there no possible routes" do
+    criteria = Criteria::CheapestCriteria.new("NLRTM", "BRSSZ")
+    routes = criteria.matching_routes
+    assert_equal 0, routes.count
+  end
+end
