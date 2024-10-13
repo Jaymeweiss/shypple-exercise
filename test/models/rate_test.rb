f5f8831a2f8fbff0ac88b11cd11de70ad9d08247
jaymeweiss <jayme.weiss@zappistore.com>
2024-10-13 19:42:40 +0200

    add placeholder tests for models

---
+++ test/models/rate_test.rb
@@ -0,0 +1,5 @@
+require "test_helper"
+
+class RateTest < ActiveSupport::TestCase
+  # This class does nothing just yet - this is a placeholder
+end
---
+++ test/models/shipping_option_test.rb
@@ -0,0 +1,5 @@
+require "test_helper"
+
+class ShippingOptionTest < ActiveSupport::TestCase
+  # This class does nothing just yet - this is a placeholder
+end
