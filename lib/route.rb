8f7a20221455a781d4296776955038fde6ef2d8c
jaymeweiss <jayme.weiss@zappistore.com>
2024-10-13 19:05:02 +0200

    set route stops accessor

--- lib/route.rb
+++ lib/route.rb
@@ -1,10 +1,13 @@
 class Route
+
+  attr_reader :stops
+
   def initialize(stops = [])
     @stops = stops
   end
 
   def price_in_euro
-    @stops.reduce(0.0) do |sum, stop|
+    stops.reduce(0.0) do |sum, stop|
       rate = Rate.find_by(sailing_code: stop.sailing_code)
       # The exchange rates are in EURO so we only convert the price if the rate is not in EURO
       if rate.rate_currency != "EUR"
@@ -18,6 +21,6 @@ class Route
 
   def duration
     # Using the assumption that we are working with whole dates and not DateTime. We can use DateTime here as well - we will just be comparing seconds to seconds instead
-    @stops.last.arrival_date - @stops.first.departure_date
+    stops.last.arrival_date - @stops.first.departure_date
   end
 end
